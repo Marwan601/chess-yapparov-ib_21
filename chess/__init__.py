627618ef5891feb6e16b9948b589483dd1f9d610
ADMIN <dilnar2014@yandex.ru>
2025-05-15 20:18:58 +0500

    2

---
+++ chess/__init__.py
@@ -0,0 +1,7 @@
+from chess.Board import Board
+from chess.Pawn import Pawn
+from chess.Rook import Rook
+from chess.King import King
+from chess.Bishop import Bishop
+
+__all__ = ['Board', 'Pawn', 'Rook', 'King', 'Bishop']
